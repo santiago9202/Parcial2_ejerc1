43a27e0a85c9c0b4cce762683eaa902eef904f42
SANTIAGO OSORIO OCHOA <santiagoosorio1120920@correo.itm.edu.co>
2024-04-05 21:00:12 -0500

    finalizamos el break segun el numero de jugadores

--- Program.cs
+++ Program.cs
@@ -13,9 +13,30 @@
                 Console.WriteLine("Por favor, ingrese el numero de jugadores ( entre 2 y 4):");
                 int.TryParse(Console.ReadLine(), out numJugadores);
 
-             } while (numJugadores > 0);
+             } while (numJugadores < 2 || numJugadores > 4);
+
+            int rangInicio = 0;
+            int rangFin = 0;
 
 
+            switch (numJugadores)
+            {
+                case 2:
+                    rangFin = 50;
+                    break;
+
+                case 3:
+                    rangFin = 100;
+                    break;
+
+                case 4:
+                    rangFin = 200;
+                    break;
+
+
+            }
+            Random random = new Random();
+            int numeroAleatorio = 
         }
     }
 }
